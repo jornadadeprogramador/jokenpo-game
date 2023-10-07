bc687648a1205dc45134b8be59c5fdc505526f6d
Jornada de Programador <contato@jornadadeprogramador.com.br>
2023-10-07 12:05:34 -0300

    Adicionado arquivo JS e as respectivas funções.

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+function jogar(jogada) {
+    console.log(jogada)
+}
+
+function jogarNovamente() {
+    console.log("Jogar novamente")
+}
+
+function zerarPlacar() {
+    console.log("Zerar placar")
+}
