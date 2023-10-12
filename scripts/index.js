a2ddcdca1c09262864e0e1e643a284a68d6dd404
Jornada de Programador <contato@jornadadeprogramador.com.br>
2023-10-12 09:56:20 -0300

    Obter a jogada da máquina de forma aleatória.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,25 @@
 function jogar(jogada) {
-    console.log(jogada)
+    
+    let jogadaMaquina = obterJogadaMaquina()
+    console.log(`${jogada} vs ${jogadaMaquina}`)
+
 }
+
+function obterJogadaMaquina() {
+    // Criar um array com as opções disponíveis
+    let opcoes = ['pedra', 'papel', 'tesoura']
+
+    // Gerar número aleatório entre 0 e 3 (não incluindo)
+    let position = getRandom(0, 3)
+
+    // Retonar a jogada da máquina
+    return opcoes[position]
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+  
 
 function jogarNovamente() {
     console.log("Jogar novamente")
